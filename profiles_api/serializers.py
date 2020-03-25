2b6f5842138792cb6b51e44e5e2bd53542bd5ff7
Juan Diego Castano <j.castano@utp.edu.co>
2020-03-25 02:23:44 -0500

    Added searching to user profile API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
+from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     """ Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+#Model Serializer
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object,
+    se configura la Meta class para que el model serializer apunte a un modelo en especifico"""
+
+    class Meta:
+        model = models.UserProfile
+         #List of fields that you want to manage through the serializer, los que quiero que sean accesibles o necesarios para create de API
+        fields = ('id', 'email', 'name', 'password')
+        # Como solo quiero password cuando creo usuario...
+        extra_kwargs = {
+            'password': {
+                'write_only': True, #Only use it to create or update, not retrieve
+                'style': {'input_type': 'password'} #To see dots when typing the password
+
+            }
+        }
+
+    # Overrides the create function
+    def create(self, validated_data):
+         """Create and return a new user"""
+         user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+         )
+
+         return user
